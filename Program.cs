088a0edfd86c1d1a3adc466156b68346be6bedc2
DougTulis <dengratulis@gmail.com>
2024-12-17 09:23:46 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projeto_ATLAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
